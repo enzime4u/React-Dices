f6cc0e7ce89bd8acf6384522d8ad9301a9ddef30
enzime4u <enzime4u@yahoo.com>
2019-07-11 15:43:38 +0300

    Updates

---
+++ precache-manifest.9e0628a6255e27565301fcc9b5060991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d3dc2dbd7a87cff51a14d921b18084",
+    "url": "/react-dices/index.html"
+  },
+  {
+    "revision": "1169458995a07c80eddb",
+    "url": "/react-dices/static/css/main.14fdfeeb.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/react-dices/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "1169458995a07c80eddb",
+    "url": "/react-dices/static/js/main.c9f80709.chunk.js"
+  },
+  {
+    "revision": "fd8f626ce9aae00a82aa",
+    "url": "/react-dices/static/js/runtime~main.798e3ea4.js"
+  }
+]);
