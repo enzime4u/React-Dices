41f2d17382581aee755d58dc1d556b5310902f82
enzime4u <enzime4u@yahoo.com>
2019-07-11 15:40:19 +0300

    adding h1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import RollDice from "./RollDice";
 function App() {
   return (
     <div className="App">
+      <h1 style={{ color: "#6c757d" }}>Click the Roll Dice button</h1>
       <RollDice />
     </div>
   );
